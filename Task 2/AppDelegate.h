3723755d768df2cec3dd992cfb9c616d78a01b61
MohamedSawy <mohamedsawy@MohamedSawys-Mac.local>
2018-05-02 02:35:27 +0200

    Initial Commit

---
+++ Task 2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Task 2
+//
+//  Created by MohamedSawy on 5/2/18.
+//  Copyright © 2018 MohamedSawy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Task 2/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Task 2
+//
+//  Created by MohamedSawy on 5/2/18.
+//  Copyright © 2018 MohamedSawy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
